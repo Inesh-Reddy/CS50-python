6a790eda4da6e6e343076b74adad09d67c211b77
Inesh-Reddy <ineshreddy007@gmail.com>
2025-01-02 19:00:25 +0530

    Implemented hello world

---
+++ 01-functions-and-variables/hello.py
@@ -0,0 +1,14 @@
+# Ask user for their name.
+name = input("What's your name? ");
+
+# say hello to user.
+print("Hello, " + name);
+print("Hello,", name); #Function arguments
+
+"""
+Comments: serves as a pseudocode.
+print is a function ..that prints Hello, World!! on to screen.
+displaying in the screen is a side Effect.
+bugs : is a mistake in program.
+return values , varaiables
+"""
